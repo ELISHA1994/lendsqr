e02064b47df91fe2ed4b13f407befcefae323c71
Elisha Bello <elishabello2014@gmail.com>
2022-10-29 02:32:53 +0100

    Docker Dev and database setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppService } from 'src/app.service';
 import { AppModule } from './app.module';
+
+function setupSwagger(app: INestApplication): void {
+  const documentBuilder = new DocumentBuilder()
+    .setTitle('Lendsqr Api')
+    .setDescription('This is lendsqr api with nest.js')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
